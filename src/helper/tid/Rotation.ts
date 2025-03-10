c0023ffcce5b85a22861e6b3ab6c3d361b24689f
Rishikant181 <rishikantsahu181@gmail.com>
2025-03-10 19:45:10 +0530

    Updated Rotation.py

--- src/helper/tid/Rotation.ts
+++ src/helper/tid/Rotation.ts
@@ -1,5 +1,5 @@
-export function convertRotationToMatrix1(rotation: number) {
-	let rad = (rotation * Math.PI) / 180;
+export function convertRotationToMatrix1(rotation: number): [number, number, number, number] {
+	const rad = (rotation * Math.PI) / 180;
 
 	return [Math.cos(rad), -Math.sin(rad), Math.sin(rad), Math.cos(rad)];
 }
