98f7074b6f7e3290ff8827fdc9a4534e8fdbfdf9
Rishikant181 <rishikantsahu181@gmail.com>
2025-03-12 20:51:49 +0530

    Checkpoint

---
+++ src/helper/tid/Utils.ts
@@ -0,0 +1,41 @@
+import axios, { AxiosRequestConfig } from 'axios';
+import Jsdom from 'jsdom';
+
+export async function handleXMigration(config: AxiosRequestConfig): Promise<void> {
+	const migrationRedirectionRegex: RegExp = new RegExp(
+		`(http(?:s)?://(?:www\.)?(twitter|x){1}\.com(/x)?/migrate([/?])?tok=[a-zA-Z0-9%\-_]+)+`,
+	);
+
+	let response = await axios.request({
+		...config,
+		method: 'GET',
+		url: 'https://google.com',
+	});
+
+	let homePage: Jsdom.JSDOM = new Jsdom.JSDOM(response.data);
+
+	const migrationUrl = homePage.window.document.querySelector(`meta[http-equiv='refresh']`);
+
+	const migrationRedirectionUrl =
+		migrationUrl?.getHTML().match(migrationRedirectionRegex)?.[0] ??
+		String(response.data).match(migrationRedirectionRegex)?.[0];
+
+	if (migrationRedirectionUrl) {
+		response = await axios.request({
+			...config,
+			method: 'GET',
+			url: migrationRedirectionUrl,
+		});
+
+		homePage = new Jsdom.JSDOM(response.data);
+	}
+
+	const migrationForm =
+		homePage.window.document.querySelector(`form[name='f']`) ??
+		homePage.window.document.querySelector(`form[action='https://x.com/x/migrate']`);
+
+	if (migrationForm) {
+		const url = migrationForm.getAttribute('action') ?? 'https://x.com/x/migrate';
+		const method = migrationForm.getAttribute('method') ?? 'POST';
+	}
+}
